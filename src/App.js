5472b7b2cb5b4fffb257bd9e617b18fcde988ab0
Vladimir <homie43@yandex.ru>
2023-11-07 16:41:32 +0300

    add async test

--- src/App.js
+++ src/App.js
@@ -1,6 +1,16 @@
+import React from 'react';
+
 function App() {
+    const [data, setData] = React.useState(null);
+    React.useEffect(() => {
+        setTimeout(() => {
+            setData({});
+        }, 100);
+    }, []);
+
     return (
         <div className='App'>
+            {data && <div style={{ color: 'red' }}>data</div>}
             <div>
                 <h1>Hello world</h1>
                 <button>Click Me</button>
--- src/App.test.js
+++ src/App.test.js
@@ -1,15 +1,24 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-    render(<App />); // тестируемый компонент
-    // через объект screen и функцию getByText мы получаем како-то элемент
-    const helloWorldElem = screen.getByText(/hello world/i);
-    const btn = screen.getByRole('button');
-    const input = screen.getByPlaceholderText(/input value/);
+describe('APP TEST', () => {
+    // test('renders learn react link', () => {
+    //     render(<App />); // тестируемый компонент
+    //     // через объект screen и функцию getByText мы получаем како-то элемент
+    //     const helloWorldElem = screen.getByText(/hello world/i);
+    //     const btn = screen.getByRole('button');
+    //     const input = screen.getByPlaceholderText(/input value/);
 
-    // toBeInTheDocument проверяет наличине ранее полученного объекта в документе
-    expect(helloWorldElem).toBeInTheDocument();
-    expect(btn).toBeInTheDocument();
-    expect(input).toBeInTheDocument();
+    //     // toBeInTheDocument проверяет наличине ранее полученного объекта в документе
+    //     expect(helloWorldElem).toBeInTheDocument();
+    //     expect(btn).toBeInTheDocument();
+    //     expect(input).toBeInTheDocument();
+    // });
+
+    test('learn react', async () => {
+        render(<App />);
+        const dataElem = await screen.findByText(/data/i);
+        expect(dataElem).toBeInTheDocument();
+        expect(dataElem).toHaveStyle({ color: 'red' });
+    });
 });
