398a452676270c7b7461d4e1ad51c155ffdeecb0
Vladimir <homie43@yandex.ru>
2023-11-07 17:40:03 +0300

    testing event: onClick, onChange

--- src/App.js
+++ src/App.js
@@ -2,6 +2,11 @@ import React from 'react';
 
 function App() {
     const [data, setData] = React.useState(null);
+    const [toggle, setToggle] = React.useState(false);
+    const [value, setValue] = React.useState('');
+
+    const onClick = () => setToggle((prev) => !prev);
+
     React.useEffect(() => {
         setTimeout(() => {
             setData({});
@@ -10,11 +15,17 @@ function App() {
 
     return (
         <div className='App'>
+            <h1 data-testid='value-elem'>{value}</h1>
+            {toggle === true && <div>ВКЛ</div>}
             {data && <div style={{ color: 'red' }}>data</div>}
             <div>
                 <h1>Hello world</h1>
-                <button>Click Me</button>
-                <input placeholder='input value' type='text' />
+                <button onClick={onClick}>Click Me</button>
+                <input
+                    onChange={(e) => setValue(e.target.value)}
+                    placeholder='input value'
+                    type='text'
+                />
             </div>
         </div>
     );
--- src/App.test.js
+++ src/App.test.js
@@ -1,4 +1,4 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen, fireEvent, userEvent } from '@testing-library/react';
 import App from './App';
 
 describe('APP TEST', () => {
@@ -15,10 +15,36 @@ describe('APP TEST', () => {
     //     expect(input).toBeInTheDocument();
     // });
 
-    test('learn react', async () => {
+    // test('learn react', async () => {
+    //     render(<App />);
+    //     const dataElem = await screen.findByText(/data/i);
+    //     expect(dataElem).toBeInTheDocument();
+    //     expect(dataElem).toHaveStyle({ color: 'red' });
+    // });
+
+    // test('learn react', () => {
+    //     render(<App />);
+    //     const btn = screen.getByRole('button');
+    //     // используем queryByText тк изначально элемента на стр нет
+    //     // проверяем что его реально нет
+    //     expect(screen.queryByText(/ВКЛ/i)).toBeNull();
+    //     // далее жмем на кнопку
+    //     fireEvent.click(btn);
+    //     // проверяем что элемент появился
+    //     expect(screen.queryByText(/ВКЛ/i)).toBeInTheDocument();
+    // });
+
+    test('test input', () => {
         render(<App />);
-        const dataElem = await screen.findByText(/data/i);
-        expect(dataElem).toBeInTheDocument();
-        expect(dataElem).toHaveStyle({ color: 'red' });
+        const input = screen.getByPlaceholderText(/input value/i);
+        // eslint-disable-next-line jest/valid-expect
+        const inputValue = screen.queryByTestId('value-elem');
+        expect(inputValue).toContainHTML('');
+        // fireEvent.input(input, {
+        //     target: { value: '12345' },
+        // });
+        userEvent.type(input, '12345');
+        // eslint-disable-next-line jest/valid-expect
+        expect(inputValue).toContainHTML('12345');
     });
 });
