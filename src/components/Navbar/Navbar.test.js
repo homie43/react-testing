c7a8a2d92698564d73eb726aed964b87fba2166d
Vladimir <homie43@yandex.ru>
2023-11-09 18:05:32 +0300

    divided the tests Navbar

--- src/components/Navbar/Navbar.test.js
+++ src/components/Navbar/Navbar.test.js
@@ -3,17 +3,24 @@ import Navbar from './Navbar';
 import { render, screen, fireEvent } from '@testing-library/react';
 
 describe('app test', () => {
-    test('renders react links', () => {
+    test('click on main page', () => {
+        render(renderWithRouter(<Navbar />));
+        const mainLink = screen.getByTestId('main-link');
+        fireEvent.click(mainLink);
+        expect(screen.getByTestId('main-page')).toBeInTheDocument();
+    });
+
+    test('click on about page', () => {
         render(renderWithRouter(<Navbar />));
         const aboutLink = screen.getByTestId('about-link');
-        const mainLink = screen.getByTestId('main-link');
-        const userstLink = screen.getByTestId('users-link');
 
         fireEvent.click(aboutLink);
         expect(screen.getByTestId('about-page')).toBeInTheDocument();
+    });
 
-        fireEvent.click(mainLink);
-        expect(screen.getByTestId('main-page')).toBeInTheDocument();
+    test('click on users page', () => {
+        render(renderWithRouter(<Navbar />));
+        const userstLink = screen.getByTestId('users-link');
 
         fireEvent.click(userstLink);
         expect(screen.getByTestId('users-page')).toBeInTheDocument();
