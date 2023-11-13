78d71d86a6b45ba4b19397acb843db69e2f8f3a8
Vladimir <homie43@yandex.ru>
2023-11-13 16:51:23 +0300

    add getCounterValue test

--- src/components/Counter/Counter.js
+++ src/components/Counter/Counter.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { getCounterValue } from '../../redux/slice/selectors/getCounterValue/getCounterValue';
-import { decrement, increment } from '../../redux/slice/counterSlice';
+import { decrement, increment, reset } from '../../redux/slice/counterSlice';
 
 const Counter = () => {
     const dispatch = useDispatch();
@@ -15,11 +15,16 @@ const Counter = () => {
         dispatch(decrement());
     };
 
+    const onReset = () => {
+        dispatch(reset());
+    };
+
     return (
         <div>
-            <h1>{value}</h1>
+            <h1>value = {value}</h1>
             <button onClick={onIncriment}>INCR</button>
             <button onClick={onDecrement}>DECR</button>
+            <button onClick={onReset}>RESET</button>
         </div>
     );
 };
--- src/redux/slice/selectors/getCounterValue/getCounterValue.test.js
+++ src/redux/slice/selectors/getCounterValue/getCounterValue.test.js
@@ -1,1 +1,17 @@
+import { getCounterValue } from './getCounterValue';
 
+describe('getCounterValue', () => {
+    test('test initial value', () => {
+        expect(getCounterValue()).toBe(0);
+    });
+
+    test('test counter value', () => {
+        expect(
+            getCounterValue({
+                counter: {
+                    value: 100,
+                },
+            })
+        ).toBe(100);
+    });
+});
